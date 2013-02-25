4a38d57846aad937e8208939ecdec9d7a8166323
markbreneman <mark.breneman@gmail.com>
2013-02-24 19:38:13 -0500

    Finished Chapter 1 Pro Media

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/b5/c0e57f14db7e00121835dbcba2d779b0
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.promedia_cameramediastore;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ ProMedia-MediaStore-Retreive/gen/com/example/promedia_mediastore_retreive/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.promedia_mediastore_retreive;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}

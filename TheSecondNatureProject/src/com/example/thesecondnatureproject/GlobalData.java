c5e5cd7c9255094ddf8dcb479110432861075097
markbreneman <mark.breneman@gmail.com>
2013-04-17 23:48:03 -0400

    adding app icon.

---
+++ TheSecondNatureProject/src/com/example/thesecondnatureproject/GlobalData.java
@@ -0,0 +1,20 @@
+package com.example.thesecondnatureproject;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+//THIS CLASS KEEPS TRACK OF WHETHER THE PHONE IS IN MOTION
+public abstract class GlobalData {
+
+    private GlobalData() {
+    };
+
+    private static final AtomicBoolean phoneInMotion = new AtomicBoolean(false);
+
+    public static boolean isPhoneInMotion() {
+        return phoneInMotion.get();
+    }
+
+    public static void setPhoneInMotion(boolean bool) {
+        phoneInMotion.set(bool);
+    }
+}
